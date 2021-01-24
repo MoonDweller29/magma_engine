beeed37c8afd672b3c5a4ab1afabafcdb2f3ea99
Lev Smirnov <lyovasmirnov@gmail.com>
2021-01-24 23:47:35 +0300

    Add .h and .cpp

---
+++ include/app/textureDump.h
@@ -0,0 +1,15 @@
+/**
+ * @file textureDump.h
+ * @author Nastormo
+ * @brief Header file that contains texture dump class
+ * @version 0.1
+ * @date 2021-01-24
+ */
+#pragma once
+
+class FrameDump {
+private:
+
+public:
+
+};
---
+++ src/app/textureDump.cpp
@@ -0,0 +1,2 @@
+#include "app/textureDump.h"
+
