4627a546c9278012705112500ca8da3c4c5a583b
Andrew Astapov <andrey.astapov@graphics.cs.msu.ru>
2021-04-01 04:26:14 +0300

    Command buffers and pool are updated

--- include/magma/vk/commands/CommandPool.h
+++ include/magma/vk/commands/CommandPool.h
@@ -1,11 +1,13 @@
 #pragma once
 #include <vulkan/vulkan.hpp>
+#include "magma/vk/vulkan_common.h"
 
 class CommandPool {
 public:
     static vk::CommandPool createPool(vk::Device device, uint32_t queueFamilyIndex);
 
     CommandPool(vk::Device device, uint32_t queueFamilyIndex);
+    NON_COPYABLE(CommandPool);
     ~CommandPool();
 
     vk::CommandPool getPool() const { return _commandPool; }
